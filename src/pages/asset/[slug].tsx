12609906e7baa8f33b8c6c268243ce46b2420c7f
JackShort <john.short.tj@gmail.com>
2023-01-26 20:26:13 -0500

    nice colors

--- src/pages/asset/[slug].tsx
+++ src/pages/asset/[slug].tsx
@@ -95,58 +95,59 @@ const Asset = () => {
                 {data.headerImageKey &&
                     <Image src={awsImages + ( data.headerImageKey as string )} width={960} height={370} alt="idk" />
                 }
-                <div className="w-full text-2xl text-white font-bold border-solid border-b-2 border-[#fa5c5c] mb-12">{data.name}</div>
+                <div className={`w-full text-2xl text-white font-bold border-solid border-b-4 border-[#fa5c5c] mb-12`}>{data.name}</div>
                 <div className="text-lg font-bold text-white mb-12">{data.description}</div>
                 {data.footerImageKey &&
                     <Image src={awsImages + ( data.footerImageKey as string )} width={960} height={480} alt="idk" />
                 }
-                {ownsAsset &&
-                    (
-                    <button className="inline-flex items-center shadow bg-[#965CFA] disabled:bg-zinc-400 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white text-lg font-bold py-2 px-10 rounded-lg" type="button" disabled={!presignedUrl.data}>
-                        <a target="_blank" href={presignedUrl.data} rel="noopener noreferrer">
-                            Download
-                        </a>
-                    </button>
-                    )
-                }
-                {!ownsAsset &&
-                    (
-                        <div className="flex flex-col">
-                            {!isLoading && (
-                                <div className="w-full text-white">
+                <div className="flex mt-12 flex-col w-full">
+                    <div className={`w-full text-2xl text-white font-bold border-solid border-b-2 border-[#fa5c5c] mb-8`}>{ownsAsset ? "Download" : "Purchase"}</div>
+                    {ownsAsset &&
+                        (
+                            <button className="inline-flex w-min items-center shadow bg-[#fa5c5c] disabled:brightness-75 hover:brightness-125 focus:shadow-outline focus:outline-none text-white text-lg font-bold py-2 px-10 rounded-lg" type="button" disabled={!presignedUrl.data}>
+                                <a target="_blank" href={presignedUrl.data} rel="noopener noreferrer">
+                                    Download
+                                </a>
+                            </button>
+                        )
+                    }
+                    {!ownsAsset &&
+                        (
+                            <div className="flex gap-8 items-center">
+                                {isConnected ?
+                                        <button className="inline-flex items-center shadow bg-[#fa5c5c] disabled:brightness-75 hover:brightness-125 focus:shadow-outline focus:outline-none text-white text-lg font-bold py-2 px-10 rounded-lg" type="button" disabled={transactionLoading || isLoading} onClick={() => write?.()}>
+                                            {isLoading ?
+                                                (
+                                                    <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                                    </svg>
+                                                )
+                                                : transactionLoading ?
+                                                    (
+                                                        <>
+                                                            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                                            </svg>
+                                                            Processing...
+                                                        </>
+                                                    )
+                                                :
+                                                    <>
+                                                        Buy Now
+                                                    </>
+                                            }
+                                        </button>
+                                    : <ConnectButton />
+                                }
+                                <div className="text-white text-xl">
                                     {ethers.utils.formatEther(BigNumber.from(data.priceInWei))} ETH
                                 </div>
-                            )}
-                            {isConnected ?
-                                    <button className="inline-flex items-center shadow bg-[#965CFA] disabled:bg-zinc-400 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white text-lg font-bold py-2 px-10 rounded-lg" type="button" disabled={transactionLoading || isLoading} onClick={() => write?.()}>
-                                        {isLoading ?
-                                            (
-                                                <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                                </svg>
-                                            )
-                                            : transactionLoading ?
-                                                (
-                                                    <>
-                                                        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                                        </svg>
-                                                        Processing...
-                                                    </>
-                                                )
-                                            :
-                                                <>
-                                                    Buy
-                                                </>
-                                        }
-                                    </button>
-                                : <ConnectButton />
-                            }
-                        </div>
-                    )
-                }
+                            </div>
+                        )
+                    }
+                </div>
             </>
         </Container>
     );
